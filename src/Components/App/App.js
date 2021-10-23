f15739706d536473f23d45b70a4a995e618620b1
Esteban <esteban.huenuman@hotmail.cl>
2021-10-24 10:41:42 +1300

    Move files - Moved to identify component - Use of React router

---
+++ src/Components/App/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import NavDrawer from '../NavDrawer';
+import WorkOrderForm from '../WorkOrder/WorkOrderForm';
+
+function App() {
+  return (
+    <Router>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <NavDrawer />
+        <Box component="main" sx={{ flexGrow: 1, p: 3, pt: 12 }}>
+          <Switch>
+            <Route exact path="/">
+              <Typography>
+                Dashboard
+              </Typography>
+            </Route>
+            <Route exact path="/new-work">
+              <WorkOrderForm />
+            </Route>
+          </Switch>
+        </Box>
+      </Box>
+    </Router>
+  );
+}
+
+export default App;
