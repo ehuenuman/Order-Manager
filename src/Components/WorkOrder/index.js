8f9708d22d2801620e3492ffcdacb171dbfa6bef
Esteban <esteban.huenuman@hotmail.cl>
2021-10-26 13:03:20 +1300

    Slit code into new component

--- src/Components/WorkOrder/index.js
+++ src/Components/WorkOrder/index.js
@@ -1,24 +1,18 @@
 import React from 'react';
+
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
 
-import AlternateEmailOutlinedIcon from '@mui/icons-material/AlternateEmailOutlined';
-import BusinessOutlinedIcon from '@mui/icons-material/BusinessOutlined';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
-import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
-import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 
 import DashboardCard from '../DashboardCard';
-import OrderStatus from './OrderStatus';
-import OrderDetails from './OrderDetails';
+import OrderStatus from './Components/OrderStatus';
+import OrderDetails from './Components/OrderDetails';
+import OrderCustomerDetails from './Components/OrderCustomerDetails';
 
 function WorkOrder() {
   return (
@@ -51,43 +45,8 @@ function WorkOrder() {
           </DashboardCard>
         </Grid>
         <Grid item xs={12} md={4}>
-          <DashboardCard
-            title="Customer details"
-          >
-            <List>
-              <ListItem>
-                <ListItemIcon>
-                  <BusinessOutlinedIcon />
-                </ListItemIcon>
-                <ListItemText
-                  primary="Business Name"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <PersonOutlinedIcon />
-                </ListItemIcon>
-                <ListItemText
-                  primary="Customer Contact Name"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <LocalPhoneOutlinedIcon />
-                </ListItemIcon>
-                <ListItemText
-                  primary="Customer Contact Number"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <AlternateEmailOutlinedIcon />
-                </ListItemIcon>
-                <ListItemText
-                  primary="Customer Contact Email"
-                />
-              </ListItem>
-            </List>
+          <DashboardCard title="Customer details">
+            <OrderCustomerDetails />
           </DashboardCard>
         </Grid>
         <Grid item xs>
