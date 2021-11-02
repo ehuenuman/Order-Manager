c5010fc1ffd54c5aa24d8c6e5b3229a6652956a6
Esteban <esteban.huenuman@hotmail.cl>
2021-11-02 17:49:58 +1300

    Resquest to fetch a customer using their ID

--- src/api/services/Customer.js
+++ src/api/services/Customer.js
@@ -1,10 +1,10 @@
-import { collection, getDocs, query, orderBy } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
 import { firestoreInstance } from '../firebase';
 
 /**
  * Fetch all the customers from Firestore
  * 
- * @return Promise <Object>[]
+ * @return Promise DocumentData[]
  */
  export async function getCustomers() {
   const customersRef = collection(firestoreInstance, 'customers');
@@ -13,4 +13,19 @@ import { firestoreInstance } from '../firebase';
     ...doc.data(),
     id: doc.id
   }));
+}
+
+/**
+ * Fetch a customers by their ID
+ * 
+ * @return Promise DocumentData 
+ */
+ export async function getCustomerById(id) {
+  const customerRef = doc(firestoreInstance, 'customers', id);
+  const querySnapshot = await getDoc(customerRef);
+  if (querySnapshot.exists()) {
+    return querySnapshot.data();
+  } else {
+    console.log('Customer does not exist, ID: ', id);
+  }
 }
