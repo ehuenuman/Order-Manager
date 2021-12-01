5cdb32696ef25a60f2e003856661ca001e8cb0f7
Esteban <esteban.huenuman@hotmail.cl>
2021-12-02 00:17:19 +1300

    New scheduled task to check the order's deadline and update their onTime status. Every day at midnight the task gets all the delayed orders.

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.checkOrderDeadline = functions.pubsub.schedule("every day 00:00")
+    .onRun((context) => {
+      const today = new Date();
+      const ordersRef = admin.firestore().collection("orders");
+      const querySnapshot = ordersRef
+          .where(
+              "status.stage",
+              "in",
+              ["isWaiting", "isOnGoing", "isReady"])
+          .where("status.onTime", "==", true)
+          .where("deadline", "<", today)
+          .get();
+
+      querySnapshot.then((delayedOrders) => {
+        if (delayedOrders.empty) {
+          functions.logger.log("No exist delayed orders so far");
+          return;
+        }
+
+        delayedOrders.forEach((doc) => {
+          ordersRef.doc(doc.id).update({
+            "status.onTime": false,
+          });
+        });
+      });
+    });
